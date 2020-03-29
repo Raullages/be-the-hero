2a22b266f72be4c3e0156d51941099f112fc679d
Raullages <raul.lages@hotmail.com>
2020-03-29 01:26:21 -0300

    finalizando o backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,17 @@
 const express = require('express')
 
+const SessionController = require('./controllers/SessionController')
+const ProfileController = require('./controllers/ProfileController')
 const OngController = require('./controllers/OngController')
 const IncidentsController = require('./controllers/IncidentsControllers')
 
 const routes = express.Router()
 
+routes.post('/session', SessionController.create)
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
+
+routes.get('/profile', ProfileController.index)
 
 routes.get('/incidents', IncidentsController.index)
 routes.post('/incidents', IncidentsController.create)
